c11fadc3480e8968af35957c14bc9c3242ff6c46
gcruzrios <gcruzr@gmail.com>
2023-08-22 14:20:04 -0600

    login updated

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,63 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const FormLogin = () => {
   return (
     <div>
-<div className="row">
-          <div className="col-lg-2">
-
-            
-          </div>
-          <div className="col-lg-10">
+      <div className="row">
+        <div className="col-lg-2"></div>
+        <div className="col-lg-10">
           <main className="form-signin w-100 m-auto">
-              <form>
-                <img
-                  className="mb-4"
-                  src="../assets/brand/bootstrap-logo.svg"
-                  alt=""
-                  width="72"
-                  height="57"
+            <form>
+              <img
+                className="mb-4"
+                src="../assets/brand/bootstrap-logo.svg"
+                alt=""
+                width="72"
+                height="57"
+              />
+              <h1 className="h3 mb-3 fw-normal text-white">Please sign in</h1>
+
+              <div className="form-floating">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
                 />
-                <h1 className="h3 mb-3 fw-normal text-white">Please sign in</h1>
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div className="form-floating">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
 
-                <div className="form-floating">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="floatingPassword"
-                    placeholder="Password"
-                  />
-                  <label for="floatingPassword">Password</label>
-                </div>
-
-                <div className="checkbox mb-3 text-white">
-                  <label>
-                    <input type="checkbox" value="remember-me"  /> Remember me
-                  </label>
-                </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit">
-                  Sign in
-                </button>
-                <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-              </form>
-              </main>
-            
-          </div>
+              <div className="checkbox mb-3 text-white">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Remember me
+                </label>
+              </div>
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Sign in
+              </button>
+              <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+            </form>
+          </main>
         </div>
-
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormLogin
+export default FormLogin;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,16 +4,35 @@ import FormLogin from "../components/FormLogin";
 const Login = () => {
   return (
     <div>
-      <div className="container text-center">
-        <div class="row">
-          <div class="col-lg-4"></div>
-          <div class="col-lg-2"></div>
-          <div class="col-lg-2"></div>
-          <div class="col-lg-4">
-            {/* <FormLogin /> */}
-          </div>
+      <main className="main-login">
+        {/* <!-- IMAGE SECTION --> */}
+        <div className="img-container">
+           <img src="/../public/images/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg" alt="Imagen de ingreso" /> 
         </div>
+        {/* <!-- INFO SECTION --> */}
+        <div className="info-login">
+          <div className="form-wrapper">
+            <h1 className="text-dark">Bienvenido</h1>
+            
+            <form class="form-signin">
+      <img class="mb-4" src="https://conapam.go.cr/wp-content/uploads/2023/02/CONAPAM_WEB.jpg" alt="" width="125" height="170"/>
+      <h1 class="h3 mb-3 font-weight-normal">Ingrese sus datos</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Su Email " required autofocus/>
+      <label for="inputPassword" class="sr-only">Contraseña</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Su Password" required/>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"/> Remember me
+        </label>
       </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
+          </div>
+          <footer className="footer">© COPYRIGHT - CONAPAM 2023</footer>
+        </div>
+      </main>
     </div>
   );
 };
